5ee7a64d03a82150481d4ddb5817ff8ed0f54351
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-11 15:41:36 +0100

    Added schema

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
-
 const app = express();
+const schema = require('./schema/schema')
+
+//middleware for handling graphql requests
+//{} takes in the Schema
+//a Schema describes what the data on our grapgh will look like.
+app.use('/graphql', graphqlHTTP({
+ schema
+}))
 
 const PORT = 4000
-
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
