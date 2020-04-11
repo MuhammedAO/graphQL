0f8fd37170c5a8917c3700dd198a874f0e550c3d
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-11 21:02:11 +0100

    resolve func

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql')
+import _ from 'lodash'
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 
@@ -6,6 +7,13 @@ const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 //define types e.g BookType
 //define relationships between types
 //define root queries which describes how a user can initally just into the graph and grab data.
+
+//dummy data
+const books = [
+ {name:'Harry Potter 1', genre:'fiction', id:'1'},
+ {name:'Harry Potter 2', genre:'fiction', id:'2'},
+ {name:'Harry Potter 3', genre:'fantasy', id:'3'}
+]
 
 //types
 const BookType = new GraphQLObjectType({
@@ -27,6 +35,7 @@ const RootQuery = new GraphQLObjectType({
    args: { id: { type: GraphQLString } },
    resolve(parent, args) {
     // code to get data from dB/other source.
+    return _.find(books, {id: args.id})
    }
   }
  }
