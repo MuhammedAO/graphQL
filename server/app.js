f2e214d9f106537fffcbcfca51cea68620693961
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-20 15:12:47 +0100

    cors

--- server/app.js
+++ server/app.js
@@ -4,24 +4,26 @@ const app = express();
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
 
+const cors = require('cors')
 
 
-
+//cors
+app.use(cors())
 
 // mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb+srv://muhammed:muhammed1234567@cluster0-hj57y.mongodb.net/test?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+mongoose.connect('mongodb+srv://muhammed:muhammed1234567@cluster0-hj57y.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 })
-.then(() => console.log('connected to DB'))
-.catch(err => console.log(err))
+  .then(() => console.log('connected to DB'))
+  .catch(err => console.log(err))
 //middleware for handling graphql requests
 //{} takes in the Schema
 //a Schema describes what the data on our grapgh will look like.
 app.use('/graphql', graphqlHTTP({
- schema,
- graphiql: true
+  schema,
+  graphiql: true
 }))
 
 const PORT = 4000
